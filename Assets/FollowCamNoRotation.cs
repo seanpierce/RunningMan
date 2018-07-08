fa7eb383276ca7dcb31793c2393b52db4116d554
seanpierce <sumler.sean@gmail.com>
2018-07-08 16:05:00 -0700

    Revmove unused assets and set camera to follow player rotation

--- Assets/FollowCamNoRotation.cs
+++ Assets/FollowCamNoRotation.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class FollowCamNoRotation : MonoBehaviour {
 
-	// reference to the player object
+	// // reference to the player object
+	// public GameObject player;
+	//
+	// // distance between camera and player
+	// // Vector3 is a refernece to xyz planes
+	// private Vector3 offset;
+	//
+	// // Use this for initialization
+	// void Start () {
+	// 	offset = transform.position - player.transform.position;
+	// }
+	//
+	// // Update is called once per frame
+	// void Update () {
+	//
+	// }
+	//
+	// // LateUpdate is better for camera movement
+	// // Late update is called after all other update functions are executed
+	// // see docs at https://docs.unity3d.com/ScriptReference/MonoBehaviour.LateUpdate.html
+	// void LateUpdate () {
+	// 	transform.position = player.transform.position + offset;
+	// }
+
 	public GameObject player;
+  public float cameraDistance = 10.0f;
 
-	// distance between camera and player
-	// Vector3 is a refernece to xyz planes
-	private Vector3 offset;
+  // Use this for initialization
+  void Start () {
+  }
 
-	// Use this for initialization
-	void Start () {
-		offset = transform.position - player.transform.position;
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	// LateUpdate is better for camera movement
-	// Late update is called after all other update functions are executed
-	// see docs at https://docs.unity3d.com/ScriptReference/MonoBehaviour.LateUpdate.html
-	void LateUpdate () {
-		transform.position = player.transform.position + offset;
-	}
+  void LateUpdate ()
+  {
+      transform.position = player.transform.position - player.transform.forward * cameraDistance;
+      transform.LookAt (player.transform.position);
+      transform.position = new Vector3 (transform.position.x, transform.position.y + 5, transform.position.z);
+  }
 }
