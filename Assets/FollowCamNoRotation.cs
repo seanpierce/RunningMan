c7c67022ed5066e1adf3e764f24b62690ebf450c
seanpierce <sumler.sean@gmail.com>
2018-07-08 16:44:27 -0700

    Add surrounding mountains and ambiant forest sounds

--- Assets/FollowCamNoRotation.cs
+++ Assets/FollowCamNoRotation.cs
@@ -2,31 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamNoRotation : MonoBehaviour {
-
-	// // reference to the player object
-	// public GameObject player;
-	//
-	// // distance between camera and player
-	// // Vector3 is a refernece to xyz planes
-	// private Vector3 offset;
-	//
-	// // Use this for initialization
-	// void Start () {
-	// 	offset = transform.position - player.transform.position;
-	// }
-	//
-	// // Update is called once per frame
-	// void Update () {
-	//
-	// }
-	//
-	// // LateUpdate is better for camera movement
-	// // Late update is called after all other update functions are executed
-	// // see docs at https://docs.unity3d.com/ScriptReference/MonoBehaviour.LateUpdate.html
-	// void LateUpdate () {
-	// 	transform.position = player.transform.position + offset;
-	// }
+public class FollowCamNoRotation : MonoBehaviour
+{
 
 	public GameObject player;
   public float cameraDistance = 10.0f;
