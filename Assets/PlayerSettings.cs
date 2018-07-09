55f11be70a5625480d5d1c314037490844ae517e
seanpierce <sumler.sean@gmail.com>
2018-07-08 18:21:49 -0700

    Add player settings script with view controls

---
+++ Assets/PlayerSettings.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+{
+
+	public GameObject player;
+	public float cameraDistance = ViewSettings.Far;
+	public int cameraHeight = 5;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	void OnGUI()
+	{
+		if (Event.current.Equals(Event.KeyboardEvent(KeyCode.V.ToString())))
+			UpdateView();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	void LateUpdate()
+	{
+		transform.position = player.transform.position - player.transform.forward * cameraDistance;
+		transform.LookAt (player.transform.position);
+		transform.position = new Vector3 (transform.position.x, transform.position.y + cameraHeight, transform.position.z);
+	}
+
+	void UpdateView()
+	{
+		if (cameraDistance == ViewSettings.Far)
+		{
+			cameraDistance = ViewSettings.Close;
+			cameraHeight = 2;
+			return;
+		}
+
+		if (cameraDistance == ViewSettings.Medium)
+		{
+			cameraDistance = ViewSettings.Far;
+			cameraHeight = 5;
+			return;
+		}
+
+		if (cameraDistance == ViewSettings.Close)
+		{
+			cameraDistance = ViewSettings.Medium;
+			cameraHeight = 4;
+			return;
+		}
+
+		print(cameraDistance);
+	}
+}
+
+public static class ViewSettings
+{
+	public static float Far = 10.0f;
+	public static float Medium = 6.0f;
+	public static float Close = 3.0f;
+}
